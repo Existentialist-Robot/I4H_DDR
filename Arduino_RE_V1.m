8b131664dd8a2f0209baeeefd8d2b49421e96d2f
Eden Redman <46800990+Existentialist-Robot@users.noreply.github.com>
2019-11-06 14:48:50 -0700

    Update Arduino_RE_V1.m

--- Arduino_RE_V1.m
+++ Arduino_RE_V1.m
@@ -3,7 +3,7 @@
     % close all
 
     % create arduino object
-a = arduino('COM7','Mega2560'); % Right USB
+a = arduino('COM5','Mega2560'); % Right USB
  
 sensorPin = 'A0';
 ledPin = 'D13';
@@ -14,13 +14,15 @@ k = 0;  %index
 v = 0;  %voltage
 t = 0;  %time
 max_index = 10000;
-
+pause_time = 0.01;
 while k < max_index
     k = k + 1;
     sensorValue = readVoltage(a,sensorPin);
-    writeDigitalPin(a, ledPin, 1);
     display(sensorValue)
-    pause(sensorValue);
+    if sensorValue >= 2
+        writeDigitalPin(a, ledPin, 1);
+    end
+    pause(pause_time);
     writeDigitalPin(a, ledPin, 0);
-    pause(sensorValue);
+
 end
