8959fc065b5f8cb6e934e5b65d761b3096408bc5
Eden Redman <eredman@ualberta.ca>
2019-11-13 13:48:31 -0700

    Update Arduino_RE_V1.m

--- Arduino_RE_V1.m
+++ Arduino_RE_V1.m
@@ -9,18 +9,27 @@ sensorPin = 'A0';
 ledPin = 'D13';
 sensorValue = 0;
 
-
 k = 0;  %index
 v = 0;  %voltage
 t = 0;  %time
 max_index = 10000;
 
+s_pause = 0.1;
+l_pause = 0.2;
+
+writeDigitalPin(a, ledPin, 0)
+
 while k < max_index
     k = k + 1;
     sensorValue = readVoltage(a,sensorPin);
-    writeDigitalPin(a, ledPin, 1);
+    if sensorValue > 2.5
+        writeDigitalPin(a, ledPin, 1);
+        if sensorValue > 3
+            pause(l_pause);
+        else
+            pause(s_pause);
+        end
+        writeDigitalPin(a, ledPin, 0);
+    end
     display(sensorValue)
-    pause(sensorValue);
-    writeDigitalPin(a, ledPin, 0);
-    pause(sensorValue);
 end
