5d6b6241caf90f231d4bda8c512801c1d4e80638
Henry Tang <hktang@ualberta.ca>
2019-11-25 15:28:30 -0700

    Arrows now falling

--- I4H_DDR/Assets/Scripts/NotesController.cs
+++ I4H_DDR/Assets/Scripts/NotesController.cs
@@ -15,7 +15,7 @@ public class NotesController : MonoBehaviour
     public GameObject downArrow;
     public GameObject upRightArrow;
     public GameObject downRightArrow;
-    public GameObject RightArrow;
+    public GameObject rightArrow;
 
     public GameObject parentObject;
 
@@ -28,7 +28,7 @@ public class NotesController : MonoBehaviour
         var currentArrow = Instantiate(leftArrow, new Vector3(leftSpawner.position.x, leftSpawner.position.y, 0f), leftSpawner.rotation);
         currentArrow.GetComponent<NoteObject>().SetBeatTempo(beatTempo);
         //currentArrow.transform.SetParent(parentObject.transform);
-        currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(5f, 5f, 5f);
     }
 
     void spawnDownLeftArrow()
@@ -36,49 +36,55 @@ public class NotesController : MonoBehaviour
         //var currentArrow = Instantiate(downLeftArrow, leftSpawner.position, leftSpawner.rotation);
         var currentArrow = Instantiate(downLeftArrow, new Vector3(leftSpawner.position.x, leftSpawner.position.y, 0f), leftSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
     }
 
     void spawnUpLeftArrow()
     {
-        var currentArrow = Instantiate(upLeftArrow, leftSpawner.position, leftSpawner.rotation);
+        //var currentArrow = Instantiate(upLeftArrow, leftSpawner.position, leftSpawner.rotation);
+        var currentArrow = Instantiate(upLeftArrow, new Vector3(leftSpawner.position.x, leftSpawner.position.y, 0f), leftSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
     }
 
     void spawnUpArrow()
     {
-        var currentArrow = Instantiate(upArrow, upSpawner.position, upSpawner.rotation);
+        // var currentArrow = Instantiate(upArrow, upSpawner.position, upSpawner.rotation);
+        var currentArrow = Instantiate(upArrow, new Vector3(upSpawner.position.x, upSpawner.position.y, 0f), upSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.0f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(5f, 5f, 5f);
     }
 
     void spawnDownArrow()
     {
-        var currentArrow = Instantiate(downArrow, downSpawner.position, downSpawner.rotation);
+        //var currentArrow = Instantiate(downArrow, downSpawner.position, downSpawner.rotation);
+        var currentArrow = Instantiate(downArrow, new Vector3(downSpawner.position.x, downSpawner.position.y, 0f), downSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.0f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(5f, 5f, 5f);
     }
 
     void spawnUpRightArrow()
     {
-        var currentArrow = Instantiate(upRightArrow, rightSpawner.position, rightSpawner.rotation);
+        //var currentArrow = Instantiate(upRightArrow, rightSpawner.position, rightSpawner.rotation);
+        var currentArrow = Instantiate(upRightArrow, new Vector3(rightSpawner.position.x, rightSpawner.position.y, 0f), rightSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
     }
 
     void spawnDownRightArrow()
     {
-        var currentArrow = Instantiate(downRightArrow, rightSpawner.position, rightSpawner.rotation);
+        //var currentArrow = Instantiate(downRightArrow, rightSpawner.position, rightSpawner.rotation);
+        var currentArrow = Instantiate(downRightArrow, new Vector3(rightSpawner.position.x, rightSpawner.position.y, 0f), rightSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
     }
 
     void spawnRightArrow()
     {
-        var currentArrow = Instantiate(RightArrow, rightSpawner.position, rightSpawner.rotation);
+        //var currentArrow = Instantiate(RightArrow, rightSpawner.position, rightSpawner.rotation);
+        var currentArrow = Instantiate(rightArrow, new Vector3(rightSpawner.position.x, rightSpawner.position.y, 0f), rightSpawner.rotation);
         currentArrow.transform.SetParent(parentObject.transform);
-        //currentArrow.transform.localScale = new Vector3(1.15f, 0.85f, 0f);
+        currentArrow.transform.localScale = new Vector3(5f, 5f, 5f);
     }
 
     public void spawnNotes(HitObject hitObject, string[] configuration, int beatTempo)
@@ -88,33 +94,33 @@ public class NotesController : MonoBehaviour
         {
             spawnLeftArrow();
         }
-        //else if (hitObject.GetX() == 109 && (((IList)configuration).Contains("0") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnDownLeftArrow();
-        //}
-        //else if (hitObject.GetX() == 182 && (((IList)configuration).Contains("6") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnUpLeftArrow();
-        //}
-        //else if (hitObject.GetX() == 224 && (((IList)configuration).Contains("7") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnUpArrow();
-        //}
-        //else if (hitObject.GetX() == 288 && (((IList)configuration).Contains("1") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnDownArrow();
-        //}
-        //else if (hitObject.GetX() == 329 && (((IList)configuration).Contains("8") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnUpRightArrow();
-        //}
-        //else if (hitObject.GetX() == 402 && (((IList)configuration).Contains("2") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnDownRightArrow();
-        //}
-        //else if (hitObject.GetX() == 475 && (((IList)configuration).Contains("3") || ((IList)configuration).Contains("4")))
-        //{
-        //    spawnRightArrow();
-        //}
+        else if (hitObject.GetX() == 109 && (((IList)configuration).Contains("0") || ((IList)configuration).Contains("4")))
+        {
+            spawnDownLeftArrow();
+        }
+        else if (hitObject.GetX() == 182 && (((IList)configuration).Contains("6") || ((IList)configuration).Contains("4")))
+        {
+            spawnUpLeftArrow();
+        }
+        else if (hitObject.GetX() == 224 && (((IList)configuration).Contains("7") || ((IList)configuration).Contains("4")))
+        {
+            spawnUpArrow();
+        }
+        else if (hitObject.GetX() == 288 && (((IList)configuration).Contains("1") || ((IList)configuration).Contains("4")))
+        {
+            spawnDownArrow();
+        }
+        else if (hitObject.GetX() == 329 && (((IList)configuration).Contains("8") || ((IList)configuration).Contains("4")))
+        {
+            spawnUpRightArrow();
+        }
+        else if (hitObject.GetX() == 402 && (((IList)configuration).Contains("2") || ((IList)configuration).Contains("4")))
+        {
+            spawnDownRightArrow();
+        }
+        else if (hitObject.GetX() == 475 && (((IList)configuration).Contains("3") || ((IList)configuration).Contains("4")))
+        {
+            spawnRightArrow();
+        }
     }
 }
