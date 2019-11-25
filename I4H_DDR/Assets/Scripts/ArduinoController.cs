f7f3da5ff35b743bf69b97652489e642f61ba39a
Henry Tang <hktang@ualberta.ca>
2019-11-24 21:20:56 -0700

    Added Arduino controller and configuration to game manager

---
+++ I4H_DDR/Assets/Scripts/ArduinoController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoController : MonoBehaviour
+{
+    public string portName;
+    SerialPort arduino;
+    private string[] configTiles;
+
+    void Start()
+    {
+        arduino = new SerialPort(portName, 9600);
+        arduino.Open();
+    }
+
+    public void UpdateGameState()
+    {
+        if (arduino.IsOpen)
+        {
+            arduino.Write("9");
+        }
+    }
+
+    public void SendConfiguration(string[] tiles)
+    {
+        configTiles = tiles;
+        StartCoroutine("SendTiles");
+    }
+
+    public void SendTile(string tile)
+    {
+        arduino.Write(tile);
+    }
+
+    IEnumerator SendTiles()
+    {
+        for(int i = 0; i < configTiles.Length; i++)
+        {
+            SendTile(configTiles[i]);
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    }
+}
