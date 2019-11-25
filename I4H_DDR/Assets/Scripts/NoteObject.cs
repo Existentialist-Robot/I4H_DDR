2a15ac3a7ead0df383a95f86e033ddfc9f44312f
ShwetaSridharan <2695+ShwetaSridharan@users.noreply.github.com>
2019-11-25 01:45:28 -0700

    Made note prefabs

---
+++ I4H_DDR/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool hit;
+    public KeyCode keyCode;
+    public int hitType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hit && Input.GetKeyDown(keyCode))
+        {
+            GameManager.Instance.NoteHit(hitType);
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "goodHitbox")
+        {
+            hitType = 1;
+            hit = true;
+        }
+        else if (collision.tag == "greatHitBox")
+        {
+            hitType = 2;
+            hit = true;
+        }
+        else if (collision.tag == "perfectHitBox")
+        {
+            hitType = 3;
+            hit = true;
+        }
+        else if(collision.tag == "despawner")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "goodHitBox")
+        {
+            hit = false;
+            hitType = 0;
+        }
+    }
+}
