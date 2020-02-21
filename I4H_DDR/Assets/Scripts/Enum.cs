7b0797d1069c0e094119116ae9d26578a5dcf4e9
Ken <>
2020-02-21 16:20:18 -0700

    add Enum for arrow

---
+++ I4H_DDR/Assets/Scripts/Enum.cs
@@ -0,0 +1,24 @@
+﻿enum ArrowActual {
+    Null = -1,
+    TopLeftDiag,
+    Top,
+    TopRightDiag,
+    Left,
+    Centre,
+    Right,
+    BottomLeftDiag,
+    Bottom,
+    BottomRightDiag
+}
+
+enum ArrowConceptual {
+    TopLeftDiag,
+    Top,
+    TopRightDiag,
+    Left,
+    Centre,
+    Right,
+    BottomLeftDiag,
+    Bottom,
+    BottomRightDiag
+}
