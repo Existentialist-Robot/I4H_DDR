88741f74c7bb46d3f26a715c651ecefb098d5480
Ken <>
2020-02-21 16:21:13 -0700

    add stuff for configurationMenu

--- I4H_DDR/Assets/Scripts/ConfigurationMenu.cs
+++ I4H_DDR/Assets/Scripts/ConfigurationMenu.cs
@@ -1,21 +1,39 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ConfigurationMenu : MonoBehaviour
 {
+    GameManager gameManager;
+
+    private void Start() {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
     public void PlayGame ()
     {
+        Debug.Log("Moving from Configuration Scene to next scene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("Moved to next scene");
     }
 
     public void Back()
     {
+        Debug.Log("Moving from Configuration Scene to previous scene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        Debug.Log("Moved to previous scene");
     }
 
+    public void ArrowButtonOnClick(int idx) {
+        bool valid = ValidateArrowConfiguration(idx);
+
+        if (valid) {
+            gameManager.UpdateArrowConfiguration(idx);
+        }
+    }
+
+    bool ValidateArrowConfiguration(int idx) {
+        // 
+
+        return false;
+    }
 }
