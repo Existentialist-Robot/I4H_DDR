3734eee8388c87434bae99988e604826ad9cf551
Ken <>
2020-02-21 16:20:56 -0700

    init arrowConfiguration in gameManager

--- I4H_DDR/Assets/Scripts/GameManager.cs
+++ I4H_DDR/Assets/Scripts/GameManager.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // REMARKS:
+    // The key (ArrowConceptual) is where physical pad placement
+    // The value (ArrowActual) is the actual pad players selected to use, ArrowActual.Null means not selected
+    Dictionary<ArrowConceptual, ArrowActual> ArrowConfiguration = new Dictionary<ArrowConceptual, ArrowActual>();
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         DontDestroyOnLoad(this.gameObject);
+
+        InitVariable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void InitVariable() {
+        // REMARKS: default ArrowConfiguration is 
+        // Arrow.Top, Arrow.Left, Arrow.Right, Arrow.Bottom
+        foreach (ArrowConceptual arrowConceptual in (ArrowConceptual[]) Enum.GetValues(typeof(ArrowConceptual))) {
+            switch (arrowConceptual) {
+                case ArrowConceptual.Top: {
+                    ArrowConfiguration[arrowConceptual] = ArrowActual.Top;
+                    break;
+                }
+                case ArrowConceptual.Left: {
+                    ArrowConfiguration[arrowConceptual] = ArrowActual.Left;
+                    break;
+                }
+                case ArrowConceptual.Right: {
+                    ArrowConfiguration[arrowConceptual] = ArrowActual.Right;
+                    break;
+                }
+                case ArrowConceptual.Bottom: {
+                    ArrowConfiguration[arrowConceptual] = ArrowActual.Bottom;
+                    break;
+                }
+                default: {
+                    ArrowConfiguration[arrowConceptual] = ArrowActual.Null;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void UpdateArrowConfiguration(int idx) {
+
     }
 }
