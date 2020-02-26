545f6a6304084b2a8f51a78e1ef2fc942efb791a
kck725 <cxwong@ualberta.ca>
2020-02-26 15:50:40 -0700

    Added difficulty scene

---
+++ I4H_DDR/Assets/Scripts/DifficultyMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyMenu : MonoBehaviour
+{
+    public GameObject[] labelArray;
+    int activeLabel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject label in labelArray)
+        {
+            label.SetActive(false);
+        }
+        labelArray[activeLabel].SetActive(true);
+    }
+
+    public void Changelabel(Slider slider)
+    {
+        labelArray[activeLabel].SetActive(false);
+        labelArray[(int)slider.value - 1].SetActive(true);
+        activeLabel = (int)slider.value - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
